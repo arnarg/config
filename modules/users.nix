b54470f0e321b30e824a85074f628cc0b1faf194
Arnar Gauti Ingason <arnarg@fastmail.com>
2020-02-01 23:24:38 +0000

    Make arnar's main group be arnar

--- modules/users.nix
+++ modules/users.nix
@@ -6,6 +6,7 @@
       arnar = {
         isNormalUser = true;
         uid = 1000;
+        group = "arnar";
         extraGroups = [ "wheel" ];
         openssh.authorizedKeys.keys = [
           "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDPa8klVyO+Tm+D5tMs9rJPhPp1wYoaLLqxXBZRddgZrNBwnWngnEwym5zpNbPfOY0L7M1t+BHwcvGxKo9TFcUQyzxCsknUuOZNpomcJrAPYHE9l+uHikhrbVuP8H0Q8nfRgiesK9jPE6lZAp4Pifo4+8ggef/w2a5ropLgnrAEmBDkFdTxmh2vBvYDY8P8KhudU3Lkr1n44Z7HX/Q8DtEjUs8aXz5oaQhO6YqBJplHfBVrZeRLuSQExovGJ9SZhLsodbfrPXeJvaBEHYtokptXfQXVeWKibs5GW+09FKN6dmqMCJaDWfHvjJ08t4/+jtGJqxTMyOYu9jhzVciu2jQkO/fMJw0iNpSFhNTIEkdrvEsiBYyrsHq0p+1mLYxI9rmDqiXstAqf1uvzC2gOmNdNk/I05Y1NLfng5ms53AnN3F+ymJizooaql6qp+oGQwIkqP1Rd9cZPMEIBX3AN5nP9ZfsxTPu1buRdBKhtRuiPZ1mJ8J1JgRuTH0u1Ds3cOI8="
@@ -16,7 +17,6 @@
     groups = {
       arnar = {
         gid = 1000;
-        members = [ "arnar" ];
       };
     };
   };
