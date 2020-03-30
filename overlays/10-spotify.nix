d794957cbd05717fef04c071a66e6b71107e4286
Arnar Gauti Ingason <arnarg@fastmail.com>
2020-03-30 19:38:10 +0000

    Converting float into a string in spotify scaling fix

--- overlays/10-spotify.nix
+++ overlays/10-spotify.nix
@@ -5,7 +5,7 @@ self: super: {
   spotify = if config.local.displayScalingFactor > 1 then super.spotify.overrideAttrs (oa: rec {
     postInstall = ''
       echo "Patch spotify.desktop to enable UI scaling"
-      sed -i "s|^Exec=spotify|Exec=spotify --force-device-scale-factor=${config.local.displayScalingFactor}|" "$out/share/applications/spotify.desktop"
+      sed -i "s|^Exec=spotify|Exec=spotify --force-device-scale-factor=${builtins.toString config.local.displayScalingFactor}|" "$out/share/applications/spotify.desktop"
     '';
   }) else super.spotify;
 
