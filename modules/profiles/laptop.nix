036aa698b593b4fc3ca6a7b11ddf0c159f965748
Arnar Gauti Ingason <arnarg@fastmail.com>
2020-03-30 17:00:50 +0000

    Switching to ondemand governor when on battery

--- modules/profiles/laptop.nix
+++ modules/profiles/laptop.nix
@@ -12,7 +12,7 @@
   services.tlp.extraConfig = ''
     TLP_DEFAULT_MODE=BAT
     CPU_SCALING_GOVERNOR_ON_AC=performance
-    CPU_SCALING_GOVERNOR_ON_BAT=powersave
+    CPU_SCALING_GOVERNOR_ON_BAT=ondemand
   '';
 
   programs.light.enable = true;
