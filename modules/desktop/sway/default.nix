5a75d59e3d403a1dc5116f164da2a3cecee5cb5b
Arnar Gauti Ingason <arnarg@fastmail.com>
2020-04-02 17:09:11 +0000

    Adding mako to sway

--- modules/desktop/sway/default.nix
+++ modules/desktop/sway/default.nix
@@ -32,7 +32,13 @@ in with pkgs.stdenv; with lib; {
   config = mkIf cfg.enable {
     environment.systemPackages = with pkgs; [
       mako
+      numix-icon-theme
     ];
+
+    # Use librsvg's gdk-pixbuf loader cache file as it enables gdk-pixbuf to load SVG files (important for icons)
+    environment.sessionVariables = {
+      GDK_PIXBUF_MODULE_FILE = "$(echo ${pkgs.librsvg.out}/lib/gdk-pixbuf-2.0/*/loaders.cache)";
+    };
 
     programs.sway.enable = true;
     users.users.arnar.extraGroups = [ "sway" ];
@@ -82,6 +88,23 @@ in with pkgs.stdenv; with lib; {
       xdg.configFile."waybar/config" = {
         text = builtins.toJSON config.local.desktop.sway.waybar.config;
         onChange = "${reloadSway}";
+      };
+
+      xdg.configFile."mako/config" = {
+        text = ''
+          font=inconsolata ${builtins.toString (config.lib.displayScaling.floor 14)}
+          background-color=#4eb5abaa
+          margin=${builtins.toString (config.lib.displayScaling.floor 14)}
+          padding=${builtins.toString (config.lib.displayScaling.floor 14)}
+          border-radius=${builtins.toString (config.lib.displayScaling.floor 10)}
+          default-timeout=5000
+          width=${builtins.toString (config.lib.displayScaling.floor 350)}
+          height=${builtins.toString (config.lib.displayScaling.floor 100)}
+          border-size=0
+          icons=1
+          max-icon-size=64
+          icon-path=${pkgs.numix-icon-theme}/share/icons/Numix
+        '';
       };
     };
   };
