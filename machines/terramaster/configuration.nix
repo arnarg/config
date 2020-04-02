64ed0e3232dec4d08960ac65234cfd9ced6ce6ef
Arnar Gauti Ingason <arnarg@fastmail.com>
2020-04-02 18:00:51 +0000

    Adding fake home-manager option for terramaster

--- machines/terramaster/configuration.nix
+++ machines/terramaster/configuration.nix
@@ -1,12 +1,14 @@
 { config, pkgs, lib, ... }:
 
 {
-  nix.nixPath = [
-    "nixpkgs=/nix/var/nix/profiles/per-user/root/channels/nixos"
-    "nixos-config=/home/arnar/Code/config/machines/terramaster/configuration.nix:/nix/var/nix/profiles/per-user/root/channels
-"
-  ];
-
+  options = {
+    # Faking home-manager as I'm not including that in this machine
+    home-manager = lib.mkOption {
+      type = lib.types.attrs;
+      default = {};
+    };
+  };
+  
   imports = [
     ../../modules
     ../../modules/os-specific/linux.nix
@@ -16,56 +18,64 @@
     ./plex.nix
   ];
 
-  # Whether to delete all files in /tmp during boot.
-  boot.cleanTmpDir = true;
-  # Use the systemd-boot EFI boot loader.
-  boot.loader.systemd-boot.enable = true;
-  # I'm booting from an external USB drive so I
-  # prefer not touching the EFI variables
-  boot.loader.efi.canTouchEfiVariables = false;
+  config = {
+    nix.nixPath = [
+      "nixpkgs=/nix/var/nix/profiles/per-user/root/channels/nixos"
+      "nixos-config=/home/arnar/Code/config/machines/terramaster/configuration.nix:/nix/var/nix/profiles/per-user/root/channels
+"
+    ];
 
-  # Terramaster f2-221 has it8613e chip
-  boot.extraModulePackages = with pkgs.linuxPackages; [ it87 pkgs.mypkgs.hddled ];
-  boot.kernelModules = ["coretemp" "it87" "hddled_tmj33"];
+    # Whether to delete all files in /tmp during boot.
+    boot.cleanTmpDir = true;
+    # Use the systemd-boot EFI boot loader.
+    boot.loader.systemd-boot.enable = true;
+    # I'm booting from an external USB drive so I
+    # prefer not touching the EFI variables
+    boot.loader.efi.canTouchEfiVariables = false;
 
-  # Enable SSH
-  services.openssh.enable = true;
+    # Terramaster f2-221 has it8613e chip
+    boot.extraModulePackages = with pkgs.linuxPackages; [ it87 pkgs.mypkgs.hddled ];
+    boot.kernelModules = ["coretemp" "it87" "hddled_tmj33"];
 
-  time.timeZone = lib.mkOverride "utc";
+    # Enable SSH
+    services.openssh.enable = true;
 
-  networking = {
-    hostName = "terramaster";
-    # Global useDHCP flag is deprecated
-    useDHCP = false;
-    interfaces = {
-      enp1s0 = {
-        useDHCP = true;
+    time.timeZone = lib.mkOverride "utc";
+
+    networking = {
+      hostName = "terramaster";
+      # Global useDHCP flag is deprecated
+      useDHCP = false;
+      interfaces = {
+        enp1s0 = {
+          useDHCP = true;
+        };
+        enp2s0 = {
+          useDHCP = true;
+        };
       };
-      enp2s0 = {
-        useDHCP = true;
+      firewall.enable = true;
+    };
+
+    # Metrics
+    services.prometheus.exporters.node.enable = true;
+    services.prometheus.exporters.node.openFirewall = true;
+    services.local.prometheus.exporters.plex.enable = true;
+    services.local.prometheus.exporters.plex.openFirewall = true;
+
+    # NixOS settings
+    system = {
+      stateVersion = "19.09";
+      autoUpgrade = {
+        enable = true;
+        dates = "Sun *-*-* 04:00:00";
       };
     };
-    firewall.enable = true;
-  };
 
-  # Metrics
-  services.prometheus.exporters.node.enable = true;
-  services.prometheus.exporters.node.openFirewall = true;
-  services.local.prometheus.exporters.plex.enable = true;
-  services.local.prometheus.exporters.plex.openFirewall = true;
-
-  # NixOS settings
-  system = {
-    stateVersion = "19.09";
-    autoUpgrade = {
-      enable = true;
-      dates = "Sun *-*-* 04:00:00";
+    nix.gc = {
+      automatic = true;
+      dates = "Mon *-*-* 06:00:00";
+      options = "--delete-older-than 35d";
     };
-  };
-
-  nix.gc = {
-    automatic = true;
-    dates = "Mon *-*-* 06:00:00";
-    options = "--delete-older-than 35d";
   };
 }
