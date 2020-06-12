f413f2f22992cfa56a5d2b377a87cdd7dff1d946
Arnar Gauti Ingason <arnarg@fastmail.com>
2020-06-12 10:58:18 +0200

    Updating zsh-nix-shell

--- modules/development/zsh/default.nix
+++ modules/development/zsh/default.nix
@@ -65,8 +65,8 @@ in with pkgs.stdenv; with lib; {
             src = fetchFromGitHub {
               owner = "chisui";
               repo = "zsh-nix-shell";
-              rev = "b2609ca787803f523a18bb9f53277d0121e30389";
-              sha256 = "01w59zzdj12p4ag9yla9ycxx58pg3rah2hnnf3sw4yk95w3hlzi6";
+              rev = "69e90b9bccecd84734948fb03087c2454a8522f6";
+              sha256 = "0snhch9hfy83d4amkyxx33izvkhbwmindy0zjjk28hih1a9l2jmx";
             };
           }
           {
