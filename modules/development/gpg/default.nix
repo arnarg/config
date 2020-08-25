969729675b6da175b8699d3a30f24b00d4ac7843
Arnar Gauti Ingason <arnarg@fastmail.com>
2020-08-25 10:52:21 +0200

    Disable scdaemon in gpg-agent

--- modules/development/gpg/default.nix
+++ modules/development/gpg/default.nix
@@ -12,7 +12,8 @@ in with lib; {
       programs.gpg.enable = true;
 
       services.gpg-agent.enable = true;
-      services.gpg-agent.pinentryFlavor = "qt";
+      services.gpg-agent.pinentryFlavor = "tty";
+      services.gpg-agent.enableScDaemon = false;
     };
 
   };
