da8eb54cd47a9c0f284cf69674163e0089480236
Arnar Gauti Ingason <arnarg@fastmail.com>
2020-09-07 10:40:29 +0200

    Move to gopass

--- modules/development/pass/default.nix
+++ modules/development/pass/default.nix
@@ -1,6 +1,7 @@
 { config, lib, pkgs, ... }:
 let
   cfg = config.local.development.pass;
+  gopass = pkgs.gopass.override { passAlias = true; };
 in with lib; {
   options.local.development.pass = {
     enable = mkEnableOption "pass";
@@ -15,6 +16,7 @@ in with lib; {
 
     home-manager.users.arnar = {
       programs.password-store.enable = true;
+      programs.password-store.package = gopass;
       programs.password-store.settings = {
         PASSWORD_STORE_KEY = cfg.gpgKey;
       };
