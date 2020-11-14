a6279c9455bdb622b81cdeb8f261e0f7d2b29252
Arnar Gauti Ingason <arnarg@fastmail.com>
2020-11-14 12:12:07 +0100

    Enabling plex on new terramaster

--- machines/terramaster/configuration.nix
+++ machines/terramaster/configuration.nix
@@ -23,8 +23,19 @@
     boot.extraModulePackages = with pkgs.linuxPackages; [ it87 pkgs.mypkgs.hddled ];
     boot.kernelModules = ["coretemp" "it87" "hddled_tmj33"];
 
+    # Enable server profile
     local.server.enable = true;
 
+    # Plex Media Server
+    services.plex.enable = true;
+    services.plex.openFirewall = true;
+    services.plex.managePlugins = false;
+    local.immutable.links.tmpfiles = [
+      "/var/lib/plex"
+    ];
+
+    # Terramaster F2-221's fan is connected to a case fan header.
+    # It doesn't spin up under load so I set up fancontrol to take care of this.
     local.services.fancontrol.enable = true;
     # Because of the order in boot.kernelModules coretemp is always loaded before it87.
     # This makes hwmon0 coretemp and hwmon1 it8613e (acpitz is hwmon2).
@@ -41,12 +52,16 @@
       MINSTOP=hwmon1/pwm3=12
     '';
 
+    # Networking stuff
     networking.hostName = "terramaster";
     networking.useDHCP = false;
     networking.interfaces.enp1s0.useDHCP = true;
 
-    system.stateVersion = "20.09";
+    # My DNS has rebinding protection and Plex doesn't like that
+    networking.nameservers = [ "1.1.1.1" "1.0.0.1" ];
 
+    # NixOS stuff
+    system.stateVersion = "20.09";
     nix.gc = {
       automatic = true;
       dates = "Mon *-*-* 06:00:00";
