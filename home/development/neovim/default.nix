83b46098f3bc55e4f6625172ed65b586c6bd4612
Arnar Gauti Ingason <arnarg@fastmail.com>
2022-01-29 13:12:11 +0100

    Add toggleterm-nvim

--- home/development/neovim/default.nix
+++ home/development/neovim/default.nix
@@ -129,6 +129,20 @@ with lib; {
           plugin = which-key-nvim;
           config = "lua require'which-key'.setup()";
         }
+        {
+          plugin = toggleterm-nvim;
+          config = ''
+            lua <<EOF
+            require'toggleterm'.setup()
+            function _G.set_terminal_keymaps()
+              local opts = {noremap = true}
+              vim.api.nvim_buf_set_keymap(0, 't', '<esc>', [[<C-\><C-n>]], opts)
+            end
+            vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
+            EOF
+            nnoremap <leader>T <cmd>ToggleTerm<cr>
+          '';
+        }
         vim-gitgutter
         vim-nix
         vim-go
