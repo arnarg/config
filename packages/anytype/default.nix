414e14ae7cb3044c735bb658d7e4aabed896a52a
Arnar Gauti Ingason <arnarg@fastmail.com>
2022-04-28 00:59:13 +0200

    Update anytype

--- packages/anytype/default.nix
+++ packages/anytype/default.nix
@@ -1,12 +1,12 @@
 { lib, stdenv, fetchurl, appimageTools, makeDesktopItem, symlinkJoin }:
 let
-  version = "0.24.0";
+  version = "0.25.0";
 
   extracted = appimageTools.extract {
     name = "anytype2";
     src = fetchurl {
       url = "https://at9412003.fra1.cdn.digitaloceanspaces.com/Anytype-${version}.AppImage";
-      sha256 = "1i4bfx4g2pl9yh39qmnx1jcd7rjicknzy3knin3h3vb7jd8v29s3";
+      sha256 = "0nwm51izyahgqaba0c7kdq4cgb0wribbmw30cmn6qmnsnxj95y3i";
     };
   };
 
