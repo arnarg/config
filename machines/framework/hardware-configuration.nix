c9c28f3706f20ead1f3d63cc8503e61611b296d2
Arnar Gauti Ingason <arnarg@fastmail.com>
2022-09-30 08:16:05 +0200

    Remove redundant stuff from framework

--- machines/framework/hardware-configuration.nix
+++ machines/framework/hardware-configuration.nix
@@ -57,11 +57,4 @@
       };
     }
   ];
-
-  hardware.opengl.extraPackages = with pkgs; [
-    vaapiIntel
-  ];
-
-  powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
-  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
