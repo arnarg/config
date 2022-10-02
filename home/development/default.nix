2488f0452bb6ab26d8886dee8aee82eabeadf635
Arnar Gauti Ingason <arnarg@fastmail.com>
2022-10-02 11:51:55 +0200

    Adding time and taskwarrior

--- home/development/default.nix
+++ home/development/default.nix
@@ -22,6 +22,7 @@
       nix-prefetch-github
       python3
       silver-searcher
+      timewarrior
       wget
       wireshark
       yubikey-manager
@@ -37,6 +38,10 @@
       ZK_NOTEBOOK_DIR = "$HOME/Documents/notes";
       LC_CTYPE = "en_US.UTF-8";
       GOPATH = "$HOME/go";
+      TIMEWARRIORDB = "$HOME/.local/share/timewarrior";
     };
+
+    # Task manager
+    programs.taskwarrior.enable = true;
   };
 }
