daad0343ecee2f176e17b6db9191bc1a0022a19c
Arnar Gauti Ingason <arnarg@fastmail.com>
2023-03-31 23:13:47 +0200

    Add extra formatters to helix

--- home/development/helix/default.nix
+++ home/development/helix/default.nix
@@ -5,13 +5,22 @@
 }: let
   # Extra packages that should be added to PATH in helix
   extraPaths = with pkgs; [
+    # Go
     gopls
     delve
+    # Python
     python311Packages.python-lsp-server
+    # Rust
     rust-analyzer
+    # YAML
     nodePackages.yaml-language-server
+    # Terraform
     terraform-ls
+    # HCL
+    hclfmt
+    # Nix
     nil
+    alejandra
   ];
 
   # Wrap helix to add packages above in PATH
@@ -39,4 +48,20 @@ in {
       indent-guides.render = true;
     };
   };
+
+  programs.helix.languages = [
+    {
+      name = "nix";
+      formatter = {
+        command = "alejandra";
+        args = ["-"];
+      };
+    }
+    {
+      name = "hcl";
+      formatter = {
+        command = "hclfmt";
+      };
+    }
+  ];
 }
