7ef4369051b0ee376f1a5f4d4396d969efde19e0
Arnar Gauti Ingason <arnarg@fastmail.com>
2024-12-27 08:19:20 +0100

    Add special permittedInsecurePackages for sonarr on terra

--- hosts/terra/configuration.nix
+++ hosts/terra/configuration.nix
@@ -15,6 +15,15 @@
       ente-cli
     ];
 
+    # For sonarr :/
+    # https://github.com/NixOS/nixpkgs/issues/360592
+    nixpkgs.config.permittedInsecurePackages = [
+      "aspnetcore-runtime-6.0.36"
+      "aspnetcore-runtime-wrapped-6.0.36"
+      "dotnet-sdk-6.0.428"
+      "dotnet-sdk-wrapped-6.0.428"
+    ];
+
     # Setup server profile.
     profiles.server.enable = true;
 
