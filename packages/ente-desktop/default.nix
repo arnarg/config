674a40b728aa1a56af616d150e1d2e05b3dd3252
Arnar Gauti Ingason <arnarg@fastmail.com>
2025-01-14 16:21:17 +0100

    Update ente photos

--- packages/ente-desktop/default.nix
+++ packages/ente-desktop/default.nix
@@ -1,16 +1,14 @@
 {
-  lib,
-  stdenv,
   fetchurl,
   appimageTools,
   makeDesktopItem,
   symlinkJoin,
 }: let
-  version = "1.7.4";
+  version = "1.7.8";
 
   src = fetchurl {
     url = "https://github.com/ente-io/photos-desktop/releases/download/v${version}/ente-${version}-x86_64.AppImage";
-    sha256 = "0mj0ag8gi5yb77h89bzhm4yc7xj9pixygj5lkbd3d0p60p8xqxg8";
+    sha256 = "sha256-cYq9YEybQsssOor/lN1k6/OVnO5l6HQgdzpFuw24q08=";
   };
 
   # Extract the appimage first so we can get the icon inside
