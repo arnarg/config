5919431c1c44287e59af3a4e8dff0dfec7d87a04
Arnar Gauti Ingason <arnarg@fastmail.com>
2025-02-25 10:48:08 +0100

    Setup NFS mount on framework

--- hosts/terra/configuration.nix
+++ hosts/terra/configuration.nix
@@ -181,10 +181,16 @@
     ################
     ## NFS Server ##
     ################
+    fileSystems."/exports/storage" = {
+      device = "/tank/SHARE/Storage";
+      options = ["bind"];
+    };
+
     services.nfs.server.enable = true;
     services.nfs.server.exports = ''
       /exports/kubernetes 192.168.0.148(rw,sync,no_root_squash)
       /exports/kubernetes 192.168.0.157(rw,sync,no_root_squash)
+      /exports/storage    192.168.0.5(rw,sync,no_root_squash)
     '';
     services.nfs.server.createMountPoints = true;
 
