c66e5e5b57ef0d26ae84f2ca88c4bd17750473c7
Arnar Gauti Ingason <arnarg@fastmail.com>
2025-03-12 22:48:30 +0100

    Update flake inputs

--- packages/ente-desktop/default.nix
+++ packages/ente-desktop/default.nix
@@ -4,11 +4,11 @@
   makeDesktopItem,
   symlinkJoin,
 }: let
-  version = "1.7.8";
+  version = "1.7.10";
 
   src = fetchurl {
     url = "https://github.com/ente-io/photos-desktop/releases/download/v${version}/ente-${version}-x86_64.AppImage";
-    sha256 = "sha256-cYq9YEybQsssOor/lN1k6/OVnO5l6HQgdzpFuw24q08=";
+    sha256 = "sha256-L2N3saNeJDdji/IzC2Zi0Iixc/pPNSUpz07egywx+4U=";
   };
 
   # Extract the appimage first so we can get the icon inside
