d1fee012b265ff1a2ae8b98dd4ee5914aa347b85
Arnar Gauti Ingason <arnarg@fastmail.com>
2025-04-05 10:05:29 +0200

    Add r4s to nilla hosts

---
+++ nilla/hosts/r4s/hardware-configuration.nix
@@ -0,0 +1,42 @@
+{
+  lib,
+  modulesPath,
+  ...
+}: {
+  imports = [
+    (modulesPath + "/installer/scan/not-detected.nix")
+  ];
+
+  nixpkgs.hostPlatform.system = "aarch64-linux";
+
+  boot.initrd.availableKernelModules = ["usb_storage" "uas"];
+  boot.initrd.kernelModules = [];
+  boot.kernelModules = [];
+  boot.extraModulePackages = [];
+
+  # Root on tmpfs
+  fileSystems."/" = {
+    device = "none";
+    fsType = "tmpfs";
+    options = ["defaults" "size=2G" "mode=755"];
+  };
+
+  fileSystems."/boot" = {
+    label = "boot";
+    fsType = "vfat";
+  };
+
+  fileSystems."/nix" = {
+    label = "nix";
+    fsType = "ext4";
+    neededForBoot = true;
+  };
+
+  fileSystems."/var/log" = {
+    device = "/nix/persist/var/log";
+    fsType = "none";
+    options = ["bind"];
+  };
+
+  powerManagement.cpuFreqGovernor = lib.mkDefault "performance";
+}
