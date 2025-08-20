530c26f3552e1ce703c0ba692c2390e324c8975e
Arnar Gauti Ingason <arnarg@fastmail.com>
2025-08-20 10:22:46 +0200

    Make crush settings configurable

--- hosts/framework/home.nix
+++ hosts/framework/home.nix
@@ -19,7 +19,6 @@
     ente-auth
     argocd
     npins
-    crush
     inputs.nilla-cli.result.packages.nilla-cli.result.x86_64-linux
     inputs.nilla-utils.result.packages.nilla-utils-plugins.result.x86_64-linux
   ];
--- modules/home/development/crush.nix
+++ modules/home/development/crush.nix
@@ -1,4 +1,10 @@
-{pkgs, ...}: let
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}: let
+  cfg = config.profiles.development.crush;
   extraPackages = with pkgs; [
     # Go
     gopls
@@ -23,100 +29,106 @@
     marksman
   ];
 in {
-  home.packages = with pkgs; [
-    (pkgs.symlinkJoin {
-      name = "${lib.getName crush}-wrapped-${lib.getVersion crush}";
-      paths = [crush];
-      preferLocalBuild = true;
-      nativeBuildInputs = [pkgs.makeWrapper];
-      postBuild = ''
-        wrapProgram $out/bin/crush \
-          --suffix PATH : ${lib.makeBinPath extraPackages}
-      '';
-    })
-  ];
+  options.profiles.development.crush = {
+    settings = lib.mkOption {
+      type = lib.types.attrs;
+      default = {};
+      description = "Settings for crush";
+    };
+  };
 
-  xdg.configFile = {
-    "crush/crush.json" = let
-      config = {
-        providers = {
-          gemini.api_key = "$(${pkgs.libsecret}/bin/secret-tool lookup llm gemini)";
+  config = {
+    home.packages = with pkgs; [
+      (pkgs.symlinkJoin {
+        name = "${lib.getName crush}-wrapped-${lib.getVersion crush}";
+        paths = [crush];
+        preferLocalBuild = true;
+        nativeBuildInputs = [pkgs.makeWrapper];
+        postBuild = ''
+          wrapProgram $out/bin/crush \
+            --suffix PATH : ${lib.makeBinPath extraPackages}
+        '';
+      })
+    ];
 
-          mistral = {
-            type = "openai";
-            base_url = "https://api.mistral.ai/v1";
-            api_key = "$(${pkgs.libsecret}/bin/secret-tool lookup llm mistral)";
-            models = [
-              {
-                id = "mistral-large-latest";
-                name = "Mistral Large";
-                cost_per_1m_in = 2;
-                cost_per_1m_out = 6;
-                context_window = 128000;
-                default_max_tokens = 48000;
-              }
-              {
-                id = "mistral-medium-latest";
-                name = "Mistral Medium";
-                cost_per_1m_in = 0.4;
-                cost_per_1m_out = 2;
-                context_window = 128000;
-                default_max_tokens = 48000;
-              }
-              {
-                id = "mistral-small-latest";
-                name = "Mistral Small";
-                cost_per_1m_in = 0.1;
-                cost_per_1m_out = 0.3;
-                context_window = 128000;
-                default_max_tokens = 48000;
-              }
-              {
-                id = "devstral-medium-2507";
-                name = "Devstral Medium";
-                cost_per_1m_in = 0.4;
-                cost_per_1m_out = 2;
-                context_window = 128000;
-                default_max_tokens = 48000;
-              }
-            ];
-          };
+    profiles.development.crush.settings = {
+      providers = {
+        gemini.api_key = "$(${pkgs.libsecret}/bin/secret-tool lookup llm gemini)";
 
-          hyperbolic = {
-            type = "openai";
-            base_url = "https://api.hyperbolic.xyz/v1";
-            api_key = "$(${pkgs.libsecret}/bin/secret-tool lookup llm hyperbolic)";
-            models = [
-              {
-                id = "deepseek-ai/DeepSeek-R1-0528";
-                name = "Deepseek R1";
-                cost_per_1m_out = 3;
-                context_window = 163795;
-                default_max_tokens = 50000;
-                can_reason = true;
-              }
-              {
-                id = "deepseek-ai/DeepSeek-V3-0324";
-                name = "Deepseek V3";
-                cost_per_1m_out = 1.25;
-                context_window = 131072;
-                default_max_tokens = 50000;
-                can_reason = false;
-              }
-              {
-                id = "moonshotai/Kimi-K2-Instruct";
-                name = "Kimi K2";
-                cost_per_1m_out = 2;
-                context_window = 131072;
-                default_max_tokens = 50000;
-                can_reason = false;
-              }
-            ];
-          };
+        mistral = {
+          type = "openai";
+          base_url = "https://api.mistral.ai/v1";
+          api_key = "$(${pkgs.libsecret}/bin/secret-tool lookup llm mistral)";
+          models = [
+            {
+              id = "mistral-large-latest";
+              name = "Mistral Large";
+              cost_per_1m_in = 2;
+              cost_per_1m_out = 6;
+              context_window = 128000;
+              default_max_tokens = 48000;
+            }
+            {
+              id = "mistral-medium-latest";
+              name = "Mistral Medium";
+              cost_per_1m_in = 0.4;
+              cost_per_1m_out = 2;
+              context_window = 128000;
+              default_max_tokens = 48000;
+            }
+            {
+              id = "mistral-small-latest";
+              name = "Mistral Small";
+              cost_per_1m_in = 0.1;
+              cost_per_1m_out = 0.3;
+              context_window = 128000;
+              default_max_tokens = 48000;
+            }
+            {
+              id = "devstral-medium-2507";
+              name = "Devstral Medium";
+              cost_per_1m_in = 0.4;
+              cost_per_1m_out = 2;
+              context_window = 128000;
+              default_max_tokens = 48000;
+            }
+          ];
+        };
+
+        hyperbolic = {
+          type = "openai";
+          base_url = "https://api.hyperbolic.xyz/v1";
+          api_key = "$(${pkgs.libsecret}/bin/secret-tool lookup llm hyperbolic)";
+          models = [
+            {
+              id = "deepseek-ai/DeepSeek-R1-0528";
+              name = "Deepseek R1";
+              cost_per_1m_out = 3;
+              context_window = 163795;
+              default_max_tokens = 50000;
+              can_reason = true;
+            }
+            {
+              id = "deepseek-ai/DeepSeek-V3-0324";
+              name = "Deepseek V3";
+              cost_per_1m_out = 1.25;
+              context_window = 131072;
+              default_max_tokens = 50000;
+              can_reason = false;
+            }
+            {
+              id = "moonshotai/Kimi-K2-Instruct";
+              name = "Kimi K2";
+              cost_per_1m_out = 2;
+              context_window = 131072;
+              default_max_tokens = 50000;
+              can_reason = false;
+            }
+          ];
         };
       };
-    in {
-      text = builtins.toJSON config;
     };
+
+    xdg.configFile."crush/crush.json".text = builtins.toJSON cfg.settings;
   };
 }
